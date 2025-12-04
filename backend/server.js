59f20bcc380e0535cc83b73d31c932182d0a5977
Ali Arshad Khan <aliarshadkhan02@gmail.com>
2025-12-05 02:48:30 +0530

    change session settings for localhost

--- backend/server.js
+++ backend/server.js
@@ -49,8 +49,8 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
